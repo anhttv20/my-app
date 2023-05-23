ad653db2ee348596bf6cdc9fa8bdf57639ea1284
anhttv20 <vanh22@yahoo.com>
2023-05-23 09:33:19 +0700

    commit 1

---
+++ src/components/FunctionComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const FunctionComponent = () => {
+    return (
+        <div>
+            <h1>Function component example!!!</h1>
+        </div>
+    );
+}
+export default FunctionComponent;
